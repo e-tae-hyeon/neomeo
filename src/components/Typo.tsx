a4a0fc46d635ec09f9e1d65c191bfc708eff4f61
e-tae-hyeon <johtta888@naver.com>
2025-03-15 14:53:31 +0900

    feat: 추가 font

---
+++ src/components/Typo.tsx
@@ -0,0 +1,87 @@
+import styled from "styled-components/native";
+
+const Bold = styled.Text.attrs({
+  lineBreakStrategyIOS: "hangul-word",
+  textBreakStrategy: "balanced",
+})`
+  color: ${(props) => props.theme.system.text100};
+  font-family: "NanumGothic";
+  font-weight: 700;
+`;
+
+const Medium = styled.Text.attrs({
+  lineBreakStrategyIOS: "hangul-word",
+  textBreakStrategy: "balanced",
+})`
+  color: ${(props) => props.theme.system.text100};
+  font-family: "NanumGothic";
+  font-weight: 500;
+`;
+const Regular = styled.Text.attrs({
+  lineBreakStrategyIOS: "hangul-word",
+  textBreakStrategy: "balanced",
+})`
+  color: ${(props) => props.theme.system.text100};
+  font-family: "NanumGothic";
+  font-weight: 400;
+`;
+
+const Title = styled(Bold)`
+  font-size: 40px;
+`;
+
+const H1 = styled(Bold)`
+  font-size: 24px;
+`;
+
+const H2 = styled(Bold)`
+  font-size: 22px;
+`;
+
+const H3 = styled(Bold)`
+  font-size: 20px;
+`;
+
+const H4 = styled(Bold)`
+  font-size: 18px;
+`;
+
+const Label = styled(Bold)`
+  font-size: 15px;
+`;
+
+const B1 = styled(Medium)`
+  font-size: 16px;
+`;
+
+const B2 = styled(Medium)`
+  font-size: 15px;
+`;
+
+const B3 = styled(Medium)`
+  font-size: 14px;
+`;
+
+const B4 = styled(Medium)`
+  font-size: 13px;
+`;
+
+const Caption = styled(Regular)`
+  font-size: 13px;
+`;
+
+const Typo = {
+  Title,
+  H1,
+  H2,
+  H3,
+  H4,
+  Label,
+  B1,
+  B2,
+  B3,
+  B4,
+  Caption,
+};
+
+export default Typo;
