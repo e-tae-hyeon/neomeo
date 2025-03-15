5bac63824baee42212404fec587fbee87aaa0325
e-tae-hyeon <johtta888@naver.com>
2025-03-15 14:58:39 +0900

    feat: 추가 firebase

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { Slot } from "expo-router";
 import { baseTheme } from "../common/theme";
+import useTrackingScreen from "../features/core/hooks/useTrackingScreen";
 
 function RootLayout() {
+  useTrackingScreen();
+
   return (
     <ThemeProvider theme={baseTheme}>
       <Slot />
---
+++ src/features/core/hooks/useTrackingScreen.ts
@@ -0,0 +1,24 @@
+import { usePathname } from "expo-router";
+import { useEffect } from "react";
+import analytics from "@react-native-firebase/analytics";
+
+analytics().setAnalyticsCollectionEnabled(!__DEV__);
+
+function useTrackingScreen() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (__DEV__) {
+      console.log(`🚀 DEV - Current Path: ${pathname}`);
+      return;
+    }
+
+    if (pathname) {
+      analytics().logScreenView({
+        screen_name: pathname,
+      });
+    }
+  }, [pathname]);
+}
+
+export default useTrackingScreen;
