309f72219e181a8a3724e480dd669ca6ca0977a5
e-tae-hyeon <johtta888@naver.com>
2025-03-15 21:29:12 +0900

    feat: 추가 auth profile screen

---
+++ src/app/auth/profile.tsx
@@ -0,0 +1,92 @@
+import Layout from "@/src/components/Layout";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components/native";
+import Cloud from "@images/core/cloud.svg";
+import Typo from "@/src/components/Typo";
+import { ScrollView } from "react-native";
+import { useRouter } from "expo-router";
+import Header from "@/src/components/Header";
+import Btn from "@/src/components/Btn";
+import usePet from "@/src/features/me/hooks/usePet";
+import Input from "@/src/components/Input";
+import useProfile from "@/src/features/auth/hooks/useProfile";
+
+function AuthProfileScreen() {
+  const { t } = useTranslation();
+  const { navigate } = useRouter();
+
+  const { profile, updateProfile } = useProfile();
+
+  const [nickname, setNickname] = useState(profile.nickname);
+  const disabled = !nickname;
+
+  const handleNext = () => {
+    updateProfile({ nickname });
+
+    navigate("/auth/kind");
+  };
+
+  return (
+    <Screen>
+      <Header.Arrow />
+
+      <ScrollView>
+        <Background>
+          <Cloud />
+        </Background>
+
+        <Main>
+          <Title>{t("auth.profile.name.title")}</Title>
+
+          <Form>
+            <Input
+              placeholder={t("auth.profile.name.placeholder")}
+              value={nickname}
+              onChangeText={setNickname}
+              onClear={() => setNickname("")}
+            />
+
+            <Caption>{t("auth.profile.name.caption")}</Caption>
+          </Form>
+        </Main>
+      </ScrollView>
+
+      <Footer>
+        <Btn onPress={handleNext} disabled={disabled}>
+          {t("common.action.next")}
+        </Btn>
+      </Footer>
+    </Screen>
+  );
+}
+
+export default AuthProfileScreen;
+
+const Screen = styled(Layout.Screen)``;
+
+const Background = styled(Layout.Center)`
+  position: absolute;
+  left: 0;
+  right: 0;
+`;
+
+const Main = styled.View`
+  gap: 20px;
+  padding: 20px;
+  padding-top: 100px;
+`;
+
+const Form = styled.View``;
+
+const Title = styled(Typo.H3)``;
+
+const Caption = styled(Typo.B3)`
+  padding: 8px 20px;
+  color: ${(props) => props.theme.gray.gray200};
+`;
+
+const Footer = styled.View`
+  padding: 20px;
+  padding-bottom: 40px;
+`;
--- src/features/auth/hooks/useProfile.ts
+++ src/features/auth/hooks/useProfile.ts
@@ -1,4 +1,6 @@
-import { useStorageObject } from "@/src/common/storage";
+import { useStorageObjectWithDefault } from "@/src/common/storage";
+import DateUtil from "@/src/util/DateUtil";
+import { generateId } from "@/src/util/generate-id";
 
 type IProfile = {
   id: string;
@@ -12,7 +14,23 @@ type IProfile = {
 function useProfile() {
   const KEY = "k_profile";
 
-  const [] = useStorageObject<IProfile>(KEY);
+  const defaultProfile: IProfile = {
+    id: generateId(),
+    nickname: "",
+    createdAt: DateUtil.nowISO(),
+    updatedAt: DateUtil.nowISO(),
+  };
+
+  const [profile, setProfile] = useStorageObjectWithDefault<IProfile>(
+    KEY,
+    defaultProfile
+  );
+
+  const updateProfile = (by: Partial<IProfile>) => {
+    setProfile({ ...profile, ...by, updatedAt: DateUtil.nowISO() });
+  };
+
+  return { profile, updateProfile };
 }
 
 export default useProfile;
