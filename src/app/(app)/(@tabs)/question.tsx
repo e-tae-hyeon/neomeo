d7441eabb8ac7d56045bde689a25e83e2ec71a79
e-tae-hyeon <johtta888@naver.com>
2025-03-17 13:31:16 +0900

    feat: 추가 background images

--- src/app/(app)/(@tabs)/question.tsx
+++ src/app/(app)/(@tabs)/question.tsx
@@ -1,11 +1,29 @@
 import Layout from "@/src/components/Layout";
+import { Image } from "expo-image";
 import React from "react";
 import styled from "styled-components/native";
 
 function QuestionScreen() {
-  return <Screen edges={["top"]}></Screen>;
+  return (
+    <Screen edges={["top"]}>
+      <Background>
+        <Image
+          source={require("@images/core/question-background.png")}
+          style={{ flex: 1 }}
+        />
+      </Background>
+    </Screen>
+  );
 }
 
 export default QuestionScreen;
 
 const Screen = styled(Layout.Screen)``;
+
+const Background = styled.View`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
