2f40271935fe2031610b0760607b319fa062df05
e-tae-hyeon <johtta888@naver.com>
2025-04-06 15:28:50 +0900

    feat: 추가 assets preload

--- src/features/core/modules/BootConfig.tsx
+++ src/features/core/modules/BootConfig.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NotiQuestionConfig from "./NotiQuestionConfig";
 import NotiConfig from "./NotiConfig";
+import PreloadAssetsConfig from "./PreloadAssetsConfig";
 
 function BootConfig() {
   return (
     <>
+      <PreloadAssetsConfig />
       <NotiConfig />
       <NotiQuestionConfig />
     </>
